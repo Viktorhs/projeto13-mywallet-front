c6548e05a5c48371b391f8c1c96252f589f62a55
Viktorhs <victor.hugo.gba@hotmail.com.br>
2022-09-12 16:54:24 -0300

    removido o context

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GlobalStyle from "../CSS/GlobalStyle"
 import Login from "./Login/Login";
-import { useState } from "react";
-import UserContext from "../contexts/UserContext";
 import Registration from "./Registration/Registration";
 import Wallet from "./Wallet/Wallet";
 import Output from "./Output/Output";
@@ -11,20 +9,16 @@ import Input from "./Input/Input";
 
 export default function App(){
 
-    const [user, setUser] = useState({})
-
     return (
-        <UserContext.Provider value={{user, setUser}}>
-            <BrowserRouter>
-                <GlobalStyle/>
-                <Routes>
-                    <Route path="/" element={<Login/>}/>
-                    <Route path="/cadastro" element={<Registration/>}/>
-                    <Route path="/wallet" element={<Wallet/>}/>
-                    <Route path="saida" element={<Output/>}/>
-                    <Route path="entrada" element={<Input/>}/>
-                </Routes>
-            </BrowserRouter>
-        </UserContext.Provider>
+        <BrowserRouter>
+            <GlobalStyle/>
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/cadastro" element={<Registration/>}/>
+                <Route path="/wallet" element={<Wallet/>}/>
+                <Route path="saida" element={<Output/>}/>
+                <Route path="entrada" element={<Input/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
